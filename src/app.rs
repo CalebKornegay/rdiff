6515898fe2566842405a4f30c828673dc7036dab
caleb.kornegay <caleb.kornegay@gmail.com>
2025-04-23 04:07:56 +0000

    Add exit and 2 boxes

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, thread::sleep, time::Duration};
 
-use ratatui::{crossterm::terminal, Terminal};
+use ratatui::{prelude::{Constraint, Direction, Layout}, Terminal, style::{Style, Color}, widgets::{Borders, Block}};
+use ratatui::crossterm::event::{self, Event, KeyCode};
 
 pub struct App {
     current_scroll: i32
@@ -12,6 +13,40 @@ impl App {
     }
 
     pub fn run<B: ratatui::backend::Backend>(&mut self, terminal: &mut Terminal<B>) -> Result<(), Box<dyn Error>> {
+        loop {
+            terminal.draw(|frame| {
+            let chunks = Layout::default()
+            .direction(Direction::Horizontal) // Arrange items horizontally
+            .constraints([
+                Constraint::Percentage(50), // First box takes 50%
+                Constraint::Percentage(50), // Second box takes the remaining 50%
+            ])
+            .split(frame.size()); // Divide the entire terminal area
+
+            let block1 = Block::default()
+                .title("Box 1")
+                .borders(Borders::ALL)
+                .style(Style::default().bg(Color::LightCyan));
+            frame.render_widget(block1, chunks[0]);
+
+            let block2 = Block::default()
+                .title("Box 2")
+                .borders(Borders::ALL)
+                .style(Style::default().bg(Color::LightGreen));
+            frame.render_widget(block2, chunks[1]);
+            })?;
+
+            // Block until an event occurs
+        if event::poll(Duration::from_millis(0))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    _ => println!("Got key {}", key.code)
+                }
+            }
+            // You can also handle other event types here, like Mouse events or Resize events
+        }
+        }
         Ok(())
     }
 }
