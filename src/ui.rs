2894327e56d4fefbb70027ce45ecf2385b1e2b2a
CalebKornegay <caleb.kornegay@gmail.com>
2025-04-28 13:18:32 -0400

    Add help screen, make center_rect static as part of class, probably should do the same for generate_block and line numbers

--- src/ui.rs
+++ src/ui.rs
@@ -6,20 +6,64 @@ pub struct Ui {
 }
 
 impl Ui {
-    pub fn new(frame: &Frame, num_boxes: u8) -> Self {
+    pub fn new(rect: Rect, num_boxes: u8) -> Self {
         Self {
             boxes: Layout::default()
                     .direction(Direction::Horizontal) // Arrange items horizontally
                     .constraints(vec![
-                        // Constraint::Percentage((100f64 / num_boxes as f64) as u16);
                         Constraint::Ratio(1, num_boxes as u32);
                         num_boxes as usize
                     ])
                     .flex(Flex::Start)
-                    .split(frame.area())
+                    .split(rect)
         }
     }
+
+    pub fn show_help<'a>(frame: &mut Frame<'a>, keybinds_text: &Vec<&str>) {
+        frame.render_widget(Paragraph::new("").block(generate_block(String::from("Help"))), frame.area());
+                    
+        let mut help_text = keybinds_text.clone().iter()
+            .map(|&l| {
+                if l == "[h] help" {
+                    Line::from("[h] to exit this screen")
+                } else {
+                    Line::from(l)
+                }
+            })
+            .collect::<Vec<Line>>();
+        help_text.extend_from_slice(
+            &[
+                Line::from("[e] end of file"),
+                Line::from("[b] begining of file"),
+                Line::from("[\u{2195}] move up and down using arrow keys or mouse"),
+                Line::from("[\u{2194}] move left and right using arrow keys")
+            ]
+        );
+
+        let b = Self::center_rect(frame.area(), Constraint::Length(help_text.iter().map(|l| l.width()).max().unwrap() as u16), Constraint::Length(help_text.len() as u16 + 2));
+        frame.render_widget(
+            Paragraph::new(help_text), b
+        );
+    }
+
+        
+    pub fn center_rect(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect {
+        let [area] = Layout::horizontal([horizontal])
+            .flex(Flex::Center)
+            .areas(area);
+        let [area] = Layout::vertical([vertical]).flex(Flex::Center).areas(area);
+        area
+    }
+
+    pub fn get_min_width(&self) -> u16 {
+        self.boxes.iter().map(|&b| b.width).min().unwrap()
+    }
+
+    pub fn get_height(&self) -> usize {
+        self.boxes.iter().map(|&b| b.height).min().unwrap() as usize
+    }
 }
+
 
 pub fn generate_block<'a>(name: String) -> Block<'a> {
     Block::default()
